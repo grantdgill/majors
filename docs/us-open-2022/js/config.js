45efd839e45f119ef89d3d530e145a64b839afda
Grant Gill <grantdgill@gmail.com>
2022-06-15 22:24:18 -0400

    us open 2022

---
+++ docs/us-open-2022/js/config.js
@@ -0,0 +1,248 @@
+var fantasy = fantasy || {},
+    RORY_MCILROY = '3470',
+    JORDAN_SPIETH = '5467',
+    JIMMY_WALKER = '446',
+    RICKIE_FOWLER = '3702',
+    JB_HOLMES = '1067',
+    PHIL_MICKELSON = '308',
+    BUBBA_WATSON = '780',
+    DUSTIN_JOHNSON = '3448',
+    JASON_DAY = '1680',
+    TIGER_WOODS = '462',
+    ADAM_SCOTT = '388',
+    HENRIK_STENSON = '576',
+    PATRICK_REED = '5579',
+    MATT_KUCHAR = '257',
+    BRANDT_SNEDEKER = '1222',
+    JUSTIN_ROSE = '569',
+    JASON_DUFNER = '110',
+    SERGIO_GARCIA = '158',
+    LEE_WESTWOOD = '455',
+    LOUIS_OOSTHUIZEN = '1293',
+    BILLY_HORSCHEL = '1651',
+    HIDEKI_MATSUYAMA = '5860',
+    ZACH_JOHNSON = '686',
+    JIM_FURYK = '153',
+    CHRIS_KIRK = '3449',
+    COREY_CONNERS = '9126',
+    CHARL_SHWARTZEL = '1097',
+    DANNY_WILLET = '4304',
+    BROOKS_KOEPKA = '6798',
+    KEVIN_KISNER = '2552',
+    JUSTIN_THOMAS = '4848',
+    BILL_HAAS = '774',
+    JON_RAHM = '9780',
+    MARC_LEISHMAN = '3351',
+    ALEX_NOREN = '3832',
+    MARTIN_KAYMER = '3670',
+    GARY_WOODLAND = '3550',
+    NIEBRUGGE = '9402',
+    THOMAS_PIETERS = '9031',
+    BRYSON_DECHAMBEAU = '10046',
+    FABRIZIO_ZANOTTI = '1770',
+    TONY_FINAU = '2230',
+    CHARLEY_HOFFMAN = '205',
+    PAUL_CASEY = '72',
+    DANIEL_BERGER = '9025',
+    KEVIN_CHAPPELL = '3857',
+    RUSSELL_HENLEY = '5409',
+    TOMMY_FLEETWOOD = '5539',
+    IAN_POULTER = '619',
+    XANDER_SCHAUFFELE = '10140',
+    KIRADECH_APHIBARNRAT = '5771',
+    FRANCESCO_MOLINARI = '1483',
+    PATRICK_CANTLAY = '6007',
+    SUNGJAE_IM = '11382',
+    COLLIN_MORIKAWA = '10592',
+    MATTHEW_FITZPATRICK = '9037',
+    WEBB_SIMPSON = '1614',
+    MATTHEW_WOLFF = '4412121',
+    CAMERON_SMITH = '9131',
+    SHANE_LOWRY = '4587',
+    CAMERON_CHAMP = '11098',
+    SCOTTIE_SCHEFFLER = '9478',
+    MAX_HOMA = '8973',
+    CARLOS_ORTIZ = '5532',
+    VIKTOR_HOVLAND = '4364873',
+    KH_LEE = '7083',
+    ABRAHAM_ANCER = '9261',
+    SAM_BURNS = '9938',
+    CHEZ_REAVIE = '769',
+    WILL_ZALATORIS = '9877',
+    TYRRELL_HATTON = '5553',
+    JOAQUIN_NIEMANN = '11099',
+    CAMERON_YOUNG = '4425906',
+    AARON_WISE = '10577';
+
+fantasy.config = {
+    tournamentName: '2022 US Open',
+    tournamentId: 401353226,
+    firstPlaceValue: 50,
+    showCut: false,
+    apiKey: '_____',
+    teams: [
+        {
+            name: 'JettaG',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                JON_RAHM,
+                COLLIN_MORIKAWA,
+                WILL_ZALATORIS
+            ]
+        },
+        {
+            name: 'JackG',
+            golfers: [
+                RORY_MCILROY,
+                JUSTIN_THOMAS,
+                SCOTTIE_SCHEFFLER,
+                COLLIN_MORIKAWA,
+                BROOKS_KOEPKA
+            ]
+        },
+        {
+            name: 'CC',
+            golfers: [
+                RORY_MCILROY,
+                JON_RAHM,
+                JORDAN_SPIETH,
+                COLLIN_MORIKAWA,
+                BROOKS_KOEPKA
+            ]
+        },
+        {
+            name: 'JohnG',
+            golfers: [
+                RORY_MCILROY,
+                JON_RAHM,
+                CAMERON_SMITH,
+                SAM_BURNS,
+                CAMERON_YOUNG
+            ]
+        },
+        {
+            name: 'TedM',
+            golfers: [
+                JORDAN_SPIETH,
+                JUSTIN_THOMAS,
+                MATTHEW_FITZPATRICK,
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY
+            ]
+        },
+        {
+            name: 'JB',
+            golfers: [
+                CAMERON_SMITH,
+                CAMERON_YOUNG,
+                RORY_MCILROY,
+                SAM_BURNS,
+                WILL_ZALATORIS
+            ]
+        },
+        {
+            name: 'CarolM',
+            golfers: [
+                XANDER_SCHAUFFELE,
+                COLLIN_MORIKAWA,
+                SCOTTIE_SCHEFFLER,
+                JORDAN_SPIETH,
+                HIDEKI_MATSUYAMA
+            ]
+        },
+        {
+            name: 'DaveG',
+            golfers: [
+                RORY_MCILROY,
+                JUSTIN_THOMAS,
+                JORDAN_SPIETH,
+                SHANE_LOWRY,
+                AARON_WISE
+            ]
+        },
+        {
+            name: 'AdamP',
+            golfers: [
+                BROOKS_KOEPKA,
+                JUSTIN_THOMAS,
+                SCOTTIE_SCHEFFLER,
+                WILL_ZALATORIS,
+                TONY_FINAU
+            ]
+        }, {
+            name: 'GrantH',
+            golfers: [
+                JON_RAHM,
+                SCOTTIE_SCHEFFLER,
+                PATRICK_CANTLAY,
+                SAM_BURNS,
+                WILL_ZALATORIS
+            ]
+        }, {
+            name: 'MatT2',
+            golfers: [
+                RORY_MCILROY,
+                JUSTIN_THOMAS,
+                PATRICK_CANTLAY,
+                SAM_BURNS,
+                SHANE_LOWRY
+            ]
+        }, {
+            name: 'MatT',
+            golfers: [
+                RORY_MCILROY,
+                JUSTIN_THOMAS,
+                CAMERON_SMITH,
+                XANDER_SCHAUFFELE,
+                WILL_ZALATORIS
+            ]
+        }, {
+            name: 'ClayT',
+            golfers: [
+                RORY_MCILROY,
+                JUSTIN_THOMAS,
+                JON_RAHM,
+                XANDER_SCHAUFFELE,
+                WILL_ZALATORIS
+            ]
+        }, {
+            name: 'SamT',
+            golfers: [
+                RORY_MCILROY,
+                JUSTIN_THOMAS,
+                SCOTTIE_SCHEFFLER,
+                XANDER_SCHAUFFELE,
+                WILL_ZALATORIS
+            ]
+        }, {
+            name: 'AngelaT',
+            golfers: [
+                RORY_MCILROY,
+                CAMERON_SMITH,
+                SCOTTIE_SCHEFFLER,
+                XANDER_SCHAUFFELE,
+                MATTHEW_FITZPATRICK
+            ]
+        }, {
+            name: 'BillT',
+            golfers: [
+                RORY_MCILROY,
+                JUSTIN_THOMAS,
+                CAMERON_SMITH,
+                JORDAN_SPIETH,
+                WILL_ZALATORIS
+                
+            ]
+        }, {
+            name: 'GrantG',
+            golfers: [
+                RORY_MCILROY,
+                JON_RAHM,
+                TONY_FINAU,
+                TOMMY_FLEETWOOD,
+                MAX_HOMA
+            ]
+        }
+    ]
+};
