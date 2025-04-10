b94b9bbabc32eecaa8c806dfa40cb4674b81fd1a
Grant Gill <grantdgill@gmail.com>
2025-04-09 23:10:10 -0400

    masters 2025

---
+++ docs/masters-2025/js/config.js
@@ -0,0 +1,265 @@
+var fantasy = fantasy || {},
+    RORY_MCILROY = '3470',
+    JORDAN_SPIETH = '5467',
+    JIMMY_WALKER = '446',
+    RICKIE_FOWLER = '3702',
+    JB_HOLMES = '1067',
+    PHIL_MICKELSON = '308',
+    BUBBA_WATSON = '780',
+    DUSTIN_JOHNSON = '3448',
+    JASON_DAY = '1680',
+    TIGER_WOODS = '462',
+    ADAM_SCOTT = '388',
+    HENRIK_STENSON = '576',
+    PATRICK_REED = '5579',
+    MATT_KUCHAR = '257',
+    BRANDT_SNEDEKER = '1222',
+    JUSTIN_ROSE = '569',
+    JASON_DUFNER = '110',
+    SERGIO_GARCIA = '158',
+    LEE_WESTWOOD = '455',
+    LOUIS_OOSTHUIZEN = '1293',
+    BILLY_HORSCHEL = '1651',
+    HIDEKI_MATSUYAMA = '5860',
+    ZACH_JOHNSON = '686',
+    JIM_FURYK = '153',
+    CHRIS_KIRK = '3449',
+    COREY_CONNERS = '9126',
+    CHARL_SHWARTZEL = '1097',
+    DANNY_WILLET = '4304',
+    BROOKS_KOEPKA = '6798',
+    KEVIN_KISNER = '2552',
+    JUSTIN_THOMAS = '4848',
+    BILL_HAAS = '774',
+    JON_RAHM = '9780',
+    MARC_LEISHMAN = '3351',
+    ALEX_NOREN = '3832',
+    MARTIN_KAYMER = '3670',
+    GARY_WOODLAND = '3550',
+    NIEBRUGGE = '9402',
+    THOMAS_PIETERS = '9031',
+    BRYSON_DECHAMBEAU = '10046',
+    FABRIZIO_ZANOTTI = '1770',
+    TONY_FINAU = '2230',
+    CHARLEY_HOFFMAN = '205',
+    PAUL_CASEY = '72',
+    DANIEL_BERGER = '9025',
+    KEVIN_CHAPPELL = '3857',
+    RUSSELL_HENLEY = '5409',
+    TOMMY_FLEETWOOD = '5539',
+    IAN_POULTER = '619',
+    XANDER_SCHAUFFELE = '10140',
+    KIRADECH_APHIBARNRAT = '5771',
+    FRANCESCO_MOLINARI = '1483',
+    PATRICK_CANTLAY = '6007',
+    SUNGJAE_IM = '11382',
+    COLLIN_MORIKAWA = '10592',
+    MATTHEW_FITZPATRICK = '9037',
+    WEBB_SIMPSON = '1614',
+    MATTHEW_WOLFF = '4412121',
+    CAMERON_SMITH = '9131',
+    SHANE_LOWRY = '4587',
+    CAMERON_CHAMP = '11098',
+    SCOTTIE_SCHEFFLER = '9478',
+    MAX_HOMA = '8973',
+    CARLOS_ORTIZ = '5532',
+    VIKTOR_HOVLAND = '4364873',
+    KH_LEE = '7083',
+    ABRAHAM_ANCER = '9261',
+    SAM_BURNS = '9938',
+    CHEZ_REAVIE = '769',
+    WILL_ZALATORIS = '9877',
+    TYRRELL_HATTON = '5553',
+    CAMERON_YOUNG = '4425906',
+    TOM_HOGE = '6086',
+    MIN_WOO_LEE = '4410932',
+    WYNDHAM_CLARK = '11119',
+    LUDVIG_ABERG = '4375972',
+    SAHITH_THEEGALA = '10980',
+    AKSHAY_BHATIA = '4419142',
+    JOAQUIN_NIEMANN = '11099',
+    DENNY_MCCARTHY = '10054',
+    JJ_SPAUN = '10166',
+    KEEGAN_BRADLEY = '4513';
+
+fantasy.config = {
+    tournamentName: '2025 Masters',
+    tournamentId: 401703504,
+    firstPlaceValue: 50,
+    showCut: false,
+    apiKey: '_____',
+    teams: [
+        {
+            name: 'JackG',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                COLLIN_MORIKAWA,
+                TOMMY_FLEETWOOD,
+                JJ_SPAUN
+            ]
+        }, {
+            name: 'JettaG',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                COLLIN_MORIKAWA,
+                JORDAN_SPIETH,
+                BRYSON_DECHAMBEAU
+            ]
+        }, {
+            name: 'JohnG',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                COLLIN_MORIKAWA,
+                XANDER_SCHAUFFELE,
+                BRYSON_DECHAMBEAU
+            ]
+        }, {
+            name: 'TedM',
+            golfers: [
+                JON_RAHM,
+                COLLIN_MORIKAWA,
+                SCOTTIE_SCHEFFLER,
+                JUSTIN_THOMAS,
+                JORDAN_SPIETH
+            ]
+        }, {
+            name: 'CarolM',
+            golfers: [
+                RORY_MCILROY,
+                BRYSON_DECHAMBEAU,
+                RUSSELL_HENLEY,
+                LUDVIG_ABERG,
+                JON_RAHM
+            ]
+        }, {
+            name: 'JB',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                WILL_ZALATORIS,
+                RUSSELL_HENLEY,
+                PATRICK_REED
+            ]
+        }, {
+            name: 'TedM',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                JON_RAHM,
+                JORDAN_SPIETH,
+                MATTHEW_FITZPATRICK
+            ]
+        }, {
+            name: 'CarolM',
+            golfers: [
+                AKSHAY_BHATIA,
+                HIDEKI_MATSUYAMA,
+                BROOKS_KOEPKA,
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER
+            ]
+        }, {
+            name: 'JB',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                BROOKS_KOEPKA,
+                HIDEKI_MATSUYAMA,
+                JOAQUIN_NIEMANN,
+                TONY_FINAU
+            ]
+        },  {
+            name: 'TommyTG',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                JUSTIN_THOMAS,
+                VIKTOR_HOVLAND,
+                PATRICK_CANTLAY
+            ]
+        }, {
+            name: 'JackTG',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                JON_RAHM,
+                TOMMY_FLEETWOOD,
+                JUSTIN_THOMAS
+            ]
+        }, {
+            name: 'MeaganG',
+            golfers: [
+                RORY_MCILROY,
+                COLLIN_MORIKAWA,
+                AKSHAY_BHATIA,
+                VIKTOR_HOVLAND,
+                TOMMY_FLEETWOOD
+            ]
+        }, {
+            name: 'BillT',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                JON_RAHM,
+                BROOKS_KOEPKA,
+                JORDAN_SPIETH
+            ]
+        }, {
+            name: 'ClayT',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                COLLIN_MORIKAWA,
+                XANDER_SCHAUFFELE,
+                HIDEKI_MATSUYAMA
+            ]
+        }, {
+            name: 'MatT',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                BRYSON_DECHAMBEAU,
+                PATRICK_CANTLAY,
+                SHANE_LOWRY
+            ]
+        }, {
+            name: 'SamT',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                BRYSON_DECHAMBEAU,
+                VIKTOR_HOVLAND,
+                RUSSELL_HENLEY
+            ]
+        }, {
+            name: 'AdamP',
+            golfers: [
+                BRYSON_DECHAMBEAU,
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                MIN_WOO_LEE,
+                JUSTIN_THOMAS
+            ]
+        }, {
+            name: 'DaveG',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                LUDVIG_ABERG,
+                COLLIN_MORIKAWA,
+                WILL_ZALATORIS,
+                RUSSELL_HENLEY
+            ]
+        }, {
+            name: 'GrantG',
+            golfers: [
+                RORY_MCILROY,
+                SCOTTIE_SCHEFFLER,
+                LUDVIG_ABERG,
+                XANDER_SCHAUFFELE,
+                KEEGAN_BRADLEY
+            ]
+        }
+    ]
+};
