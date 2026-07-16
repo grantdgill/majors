ae06ea9761dec5ff3b7c1cdda00d4db2f180151c
Grant Gill <grantdgill@gmail.com>
2026-07-16 06:53:44 -0400

    open championship 2026

---
+++ docs/british-open-2026/js/config.js
@@ -0,0 +1,240 @@
+var fantasy = fantasy || {},
+    RORY_MCILROY = '3470',
+    JORDAN_SPIETH = '5467',
+    JIMMY_WALKER = '446',
+    RICKIE_FOWLER = '3702',
+    JB_HOLMES = '1067',
+    PHIL_MICKELSON = '308',
+    BUBBA_WATSON = '780',
+    DUSTIN_JOHNSON = '3448',
+    JASON_DAY = '1680',
+    TIGER_WOODS = '462',
+    ADAM_SCOTT = '388',
+    HENRIK_STENSON = '576',
+    PATRICK_REED = '5579',
+    MATT_KUCHAR = '257',
+    BRANDT_SNEDEKER = '1222',
+    JUSTIN_ROSE = '569',
+    JASON_DUFNER = '110',
+    SERGIO_GARCIA = '158',
+    LEE_WESTWOOD = '455',
+    LOUIS_OOSTHUIZEN = '1293',
+    BILLY_HORSCHEL = '1651',
+    HIDEKI_MATSUYAMA = '5860',
+    ZACH_JOHNSON = '686',
+    JIM_FURYK = '153',
+    CHRIS_KIRK = '3449',
+    COREY_CONNERS = '9126',
+    CHARL_SHWARTZEL = '1097',
+    DANNY_WILLET = '4304',
+    BROOKS_KOEPKA = '6798',
+    KEVIN_KISNER = '2552',
+    JUSTIN_THOMAS = '4848',
+    BILL_HAAS = '774',
+    JON_RAHM = '9780',
+    MARC_LEISHMAN = '3351',
+    ALEX_NOREN = '3832',
+    MARTIN_KAYMER = '3670',
+    GARY_WOODLAND = '3550',
+    NIEBRUGGE = '9402',
+    THOMAS_PIETERS = '9031',
+    BRYSON_DECHAMBEAU = '10046',
+    FABRIZIO_ZANOTTI = '1770',
+    TONY_FINAU = '2230',
+    CHARLEY_HOFFMAN = '205',
+    PAUL_CASEY = '72',
+    DANIEL_BERGER = '9025',
+    KEVIN_CHAPPELL = '3857',
+    RUSSELL_HENLEY = '5409',
+    TOMMY_FLEETWOOD = '5539',
+    IAN_POULTER = '619',
+    XANDER_SCHAUFFELE = '10140',
+    KIRADECH_APHIBARNRAT = '5771',
+    FRANCESCO_MOLINARI = '1483',
+    PATRICK_CANTLAY = '6007',
+    SUNGJAE_IM = '11382',
+    COLLIN_MORIKAWA = '10592',
+    MATTHEW_FITZPATRICK = '9037',
+    WEBB_SIMPSON = '1614',
+    MATTHEW_WOLFF = '4412121',
+    CAMERON_SMITH = '9131',
+    SHANE_LOWRY = '4587',
+    CAMERON_CHAMP = '11098',
+    SCOTTIE_SCHEFFLER = '9478',
+    MAX_HOMA = '8973',
+    CARLOS_ORTIZ = '5532',
+    VIKTOR_HOVLAND = '4364873',
+    KH_LEE = '7083',
+    ABRAHAM_ANCER = '9261',
+    SAM_BURNS = '9938',
+    CHEZ_REAVIE = '769',
+    WILL_ZALATORIS = '9877',
+    TYRRELL_HATTON = '5553',
+    CAMERON_YOUNG = '4425906',
+    TOM_HOGE = '6086',
+    MIN_WOO_LEE = '4410932',
+    WYNDHAM_CLARK = '11119',
+    LUDVIG_ABERG = '4375972',
+    SAHITH_THEEGALA = '10980',
+    AKSHAY_BHATIA = '4419142',
+    JOAQUIN_NIEMANN = '11099',
+    DENNY_MCCARTHY = '10054',
+    JJ_SPAUN = '10166',
+    KEEGAN_BRADLEY = '4513',
+    PATRICK_RODGERS = '6825',
+    MICHAEL_THORBJORNSEN = '4425904',
+    JAKE_KNAPP = '9843',
+    MAVERICK_MCNEALY = '9530',
+    SEPP_STRAKA = '8961',
+    SI_WOO_KIM = '7081',
+    ROBERT_MACINTYRE = '11378',
+    CHRIS_GOTTERUP = '4690755',
+    KURT_KITAYAMA = '10364',
+    ALEX_FITZPATRICK = '4364865',
+    TOM_KIM = '4602673';
+
+fantasy.config = {
+    tournamentName: '2026 Open Championship',
+    tournamentId: 401811957,
+    firstPlaceValue: 50,
+    showCut: false,
+    apiKey: '_____',
+    teams: [
+        {
+            name: 'JohnG',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                MATTHEW_FITZPATRICK,
+                TOMMY_FLEETWOOD,
+                SAM_BURNS,
+                PATRICK_REED
+            ]
+        }, {
+            name: 'JackG',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                CAMERON_YOUNG,
+                JUSTIN_ROSE,
+                COLLIN_MORIKAWA
+            ]
+        }, {
+            name: 'TedM',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                TOMMY_FLEETWOOD,
+                XANDER_SCHAUFFELE,
+                LUDVIG_ABERG
+            ]
+        }, {
+            name: 'CarolM',
+            golfers: [
+                BROOKS_KOEPKA,
+                RORY_MCILROY,
+                MATTHEW_FITZPATRICK,
+                SCOTTIE_SCHEFFLER,
+                CHRIS_GOTTERUP
+            ]
+        }, {
+            name: 'DaveG',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                JON_RAHM,
+                COLLIN_MORIKAWA,
+                XANDER_SCHAUFFELE
+            ]
+        }, {
+            name: 'MeaganG',
+            golfers: [
+                RORY_MCILROY,
+                TOMMY_FLEETWOOD,
+                COLLIN_MORIKAWA,
+                VIKTOR_HOVLAND,
+                AKSHAY_BHATIA
+            ]
+        }, {
+            name: 'TommyG',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                MATTHEW_FITZPATRICK,
+                CAMERON_YOUNG,
+                JON_RAHM,
+                LUDVIG_ABERG
+            ]
+        }, {
+            name: 'JackieG',
+            golfers: [
+                JUSTIN_ROSE,
+                MATTHEW_FITZPATRICK,
+                VIKTOR_HOVLAND,
+                COLLIN_MORIKAWA,
+                BROOKS_KOEPKA
+            ]
+        }, {
+            name: 'AdamP',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                MATTHEW_FITZPATRICK,
+                TOM_KIM,
+                SHANE_LOWRY,
+                JUSTIN_ROSE
+            ]
+        }, {
+            name: 'AdamP2',
+            golfers: [
+                RORY_MCILROY,
+                JON_RAHM,
+                TOMMY_FLEETWOOD,
+                TOM_KIM,
+                JUSTIN_THOMAS
+            ]
+        }, {
+            name: 'GrantG',
+            golfers: [
+                TOMMY_FLEETWOOD,
+                RORY_MCILROY,
+                JORDAN_SPIETH,
+                MATTHEW_FITZPATRICK,
+                MIN_WOO_LEE
+            ]
+        }, {
+            name: 'MatT',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                MATTHEW_FITZPATRICK,
+                JUSTIN_ROSE,
+                SAM_BURNS
+            ]
+        }, {
+            name: 'ClayT',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                MATTHEW_FITZPATRICK,
+                RORY_MCILROY,
+                JUSTIN_ROSE,
+                COLLIN_MORIKAWA
+            ]
+        }, {
+            name: 'SamT',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                ALEX_FITZPATRICK,
+                RORY_MCILROY,
+                TYRRELL_HATTON,
+                TOMMY_FLEETWOOD
+            ]
+        }, {
+            name: 'BillT',
+            golfers: [
+                SCOTTIE_SCHEFFLER,
+                RORY_MCILROY,
+                ALEX_FITZPATRICK,
+                JON_RAHM,
+                JUSTIN_ROSE
+            ]
+        }
+    ]
+};
